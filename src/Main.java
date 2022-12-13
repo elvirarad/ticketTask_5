d2c658cc3193e6810e99889f748aaf2303ddad4d
Elvira <rostechno_rb@mail.ru>
2022-12-14 02:01:52 +0500

    Problem solved correct 1

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         String[][] book = {{"Война миров", "Гэрберт Уэлс"}, {"Судьба человека", "Михаил Александрович Шолохов"},{"Понедельник начинается в субботу", "Стругацкие"}, {"Посёлок", "Кир Булычёв"},{"Идиот", "Федр Михайлович Достоевский"}};
-        int k;
-        for (int i = 0; i < book.length; i++) {
-            System.out.print("<<" + book[i][0] + ">> ");
-                String authtor[];
-                authtor = book[i][1].split(" ");
+        int k = 0;
+        int i = 0;
+        StringBuilder[] bookAuthtor = new StringBuilder[book.length];
+        while (i < book.length) {
+            bookAuthtor[i] = new StringBuilder("<<");
+            bookAuthtor[i].append(book[i][0]);
+            bookAuthtor[i].append(">> ");
+                String  authtor[] = book[i][1].split(" ");
                 for (k = 0; k < authtor.length - 1; k++) {
                     char name = authtor[k].charAt(0);
-                    System.out.print(name + ".");
+                    bookAuthtor[i].append(name);
+                    bookAuthtor[i].append(".");
                 }
-                String surName = authtor[k];
-                System.out.print(surName);
-                System.out.println();
+            bookAuthtor[i].append(authtor[k]);
+            i++;
             }
+        for (StringBuilder stringBuilder : bookAuthtor) {
+            System.out.println(stringBuilder);
         }
-}
+        }
+    }
